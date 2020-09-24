bfe76abdc3d058fb20a285128b868c28e66cca62
I-Am-Lain <vetter.william@gmail.com>
2020-09-23 23:54:05 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+  def kind
+
+    raise TriangleError if a + b < c || b + c < a || a + c < b # not a triangle!
+    raise TriangleError if a + b == c || b + c == a || a + c == b # degenerate triangle!
+  
+    return :equilateral  if a == b && a == c
+    return :isosceles    if a == b || b == c || a == c
+    return :scalene      if a != b && b != c && a != c
+  
+  end
+
+
+  class TriangleError < StandardError
+  end
+
 end
